06c03c7bdf62fff511f28b3acc807e4ea4d1a171
zachary <zhuba@my.wctc.edu>
2020-10-27 15:09:24 -0500

    Version 2

---
+++ code.js
@@ -0,0 +1,16 @@
+function sayHello()
+{
+    document.write("this is some javascript");
+}
+
+function rollDie()
+{
+    var die = Math.floor(Math.random() * 6) + 1;
+    alert(die);
+}
+
+function drawCard()
+{
+    var card = Math.floor(Math.random() * 52) + 1;
+    alert(card);
+}
